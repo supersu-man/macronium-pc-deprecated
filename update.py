43ab3e4abca5f923256c4dd2c06dd6102006b5dc
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-11-15 16:38:07 +0530

    Fixed update issue

--- update.py
+++ update.py
@@ -1,15 +1,14 @@
 import requests
-
+from bs4 import BeautifulSoup
 
 def get_latest(url):
-    response = requests.get(url)
-    temp = response.text.split("label-latest")[1].split("list-style-none")[1]
-    latest = temp.split("title=\"")[1].split("\"")[0]
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
+    latestversion = soup.find_all(lambda tag: tag.name == 'span' and tag.get('class') == ['ml-1'])
+    latest = latestversion[0].text.strip()
     if "v" in latest:
         latest = latest.replace("v", "")
-
     return latest
-
 
 def isInternet():
     try:
